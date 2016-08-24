5274956000ee8100128494b4969104d95d9e4961
HoneyBadgered <Honeybadgeredhoney@gmail.com>
2016-08-24 10:07:14 -0400

    Update topinclude.php

--- topinclude.php
+++ topinclude.php
@@ -1,10 +1,10 @@
 <?php
 //session_start();
 //ob_start();
-$servername 	= "localhost";
-$username 		= "root";
-$password 		= "root";
-$db_name 		= "dbtest";
+$servername 	= "******";
+$username 		= "****";
+$password 		= "****";
+$db_name 		= "******";
 $conn = mysqli_connect($servername, $username, $password, $db_name);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
